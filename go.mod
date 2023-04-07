75f20080db0e07f20700cd3300291438676e7c4e
JrGustavo <gus@soypetal.com>
2023-04-06 21:02:11 -0500

    Update index

---
+++ go.mod
@@ -0,0 +1,3 @@
+module CLIGoLang
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	task "CLIGoLang/tasks"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("task.json", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	var tasks []task.Task
+
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if info.Size() != 0 {
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(bytes, &tasks)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		tasks = []task.Task{}
+	}
+
+	fmt.Println(tasks)
+}
